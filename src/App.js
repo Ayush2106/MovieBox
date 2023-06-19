ba6119411f8ea96d43b615276fe2dc02145e64dc
Ayush Singla <ayush21singla@gmail.com>
2023-06-19 21:34:43 +0530

    update folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner';
-// import Navbar from './Navbar';
-import Footer from './Footer/Footer';
+import "./App.css";
+import Row from "./components/Row/Row";
+import requests from "./components/requests";
+import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-     {/* <Navbar/> */}
-     <Banner/>
-        <Row title = "Originals"  isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-        <Row  title = "Top Rated" fetchUrl={requests.fetchTopRated}/>
-        <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-        <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-        <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-        <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries}/>
-<Footer/>
+      <Navbar />
+      <Banner />
+      <Row
+        title="Originals"
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ function Navbar() {
 <img  className = "nav_logo"  src="https://149695847.v2.pressablecdn.com/wp-content/uploads/2020/03/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png"
    alt = "netflix_logo" />
 
+<h2>Rent </h2>
    <img className='nav_avatar' src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAEy-Hi3lntqni03_IgMHV_6nbWR5sG5EuE11oKCej1YqlHvxzo6lfyF7L_JXrJaoZIkY&usqp=CAU"
    alt = "avatar"/>
     </div>
